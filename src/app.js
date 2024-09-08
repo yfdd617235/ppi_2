f3e4af2fdcc2f8eeafbc38786c50237861b75209
Yosef David Giraldo Salazar <yosefdavid91@gmail.com>
2024-09-07 20:11:05 -0500

    update token

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import morgan from "morgan";
+import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cookieParser());
 
-app.use('/api',authRoutes); //Las rutas empesarían con api
+app.use('/api', authRoutes); //Las rutas empesarían con api
 
 export default app;
