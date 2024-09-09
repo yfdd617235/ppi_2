1cb30b10bfa466c8e8bba18d3e64de032b15f2ec
yfdd617235 <yosefdavid91@gmail.com>
2024-09-09 15:52:02 -0500

    register page frontend

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,40 @@
+import { useForm } from 'react-hook-form'
+import { registerRequest } from '../api/auth';
+
+function RegisterPage() {
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = handleSubmit(async (values) => {
+        //console.log(values)
+        const res = await registerRequest(values)
+        console.log(res)
+    })
+
+    return (
+        <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
+
+            <form
+                onSubmit={onSubmit}>
+
+                <input type='text' {...register("username", { required: true })}
+                    className='w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md'
+                    placeholder='Username' />
+
+                <input type='email' {...register("email", { required: true })}
+                    className='w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md'
+                    placeholder='Email' />
+
+                <input type='password' {...register("password", { required: true })}
+                    className='w-full bg-zinc-700 text-white px-4 py-2 my-2 rounded-md'
+                    placeholder='Password' />
+
+                <button type='submit'>Register</button>
+
+            </form>
+
+        </div>
+    );
+}
+
+export default RegisterPage;
