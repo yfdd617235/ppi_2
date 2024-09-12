4cee391ef709ab2ac9686588b2b4f0c3601ab2c1
yfdd617235 <yosefdavid91@gmail.com>
2024-09-12 15:58:23 -0500

    conect mongo atlas

--- src/db.js
+++ src/db.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://PPI:PPI123@cluster0.kgxfus6.mongodb.net/?retryWrites=true&w=majority');
-        console.log(">>> Mongo ATLAS is Conected");
+        await mongoose.connect('mongodb+srv://PPI:PPI123@cluster0.kgxfus6.mongodb.net/ppi2?retryWrites=true&w=majority');
+        console.log(">>> Mongo ATLAS is Connected");
 
     } catch (error) {
         console.log(error);
