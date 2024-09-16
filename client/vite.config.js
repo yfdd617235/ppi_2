0c0b28cd1f4ca52807afad7ee6dfa8033d1baedc
yfdd617235 <yosefdavid91@gmail.com>
2024-09-16 16:34:52 -0500

    update run deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://yfdd617235.github.io/ppi_2', // Ruta base relativa
+  base: 'https://yfdd617235.github.io/ppi_2/', // Ruta base relativa
 });
