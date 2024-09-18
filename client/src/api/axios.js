d0dc8e58781255b4a0ced2d50b3c1f26017a9bfd
yfdd617235 <yosefdavid91@gmail.com>
2024-09-18 18:17:15 -0500

    update true

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://ppi-2.onrender.com/api' || 'http://localhost:3000/api', // Si no está definida la primera URL, usa localhost
-    withCredentials: false
+    withCredentials: true
 });
 
 export default instance;
