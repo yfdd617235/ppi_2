bf77b3e9be74135ae44e918224dfb3e9e63c3407
yfdd617235 <yosefdavid91@gmail.com>
2024-09-18 16:49:20 -0500

    START SCRIPT

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import app from './app.js'
 import { connectDB } from './db.js'
 
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+  });
+
 connectDB();
 app.listen(3000, ()=>{
     console.log('>>> Server on port', 3000)
