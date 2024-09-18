9c5eec9dbd1016d7455f51dde08ca996ef7987d2
yfdd617235 <yosefdavid91@gmail.com>
2024-09-18 16:57:22 -0500

    update port

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { connectDB } from './db.js';
 // Conectar a la base de datos
 connectDB();
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+  });
+
 // Utiliza el puerto que asigna Render o, en su defecto, el puerto 3000 para desarrollo local
 const PORT = process.env.PORT || 3000;
 
