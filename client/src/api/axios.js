1bfbc68cb1b922d96969b39d0827401706976d17
Yosef David Giraldo Salazar <yosefdavid91@gmail.com>
2024-09-18 20:30:37 -0500

    update

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,17 @@
+// import axios from 'axios';
+
+// const instance = axios.create({
+//     baseURL: 'http://localhost:3000/api', // Si no está definida la primera URL, usa localhost
+//     withCredentials: true
+// });
+
+// export default instance;
+
+
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://ppi-2.onrender.com/api' || 'http://localhost:3000/api', // Si no está definida la primera URL, usa localhost
+    baseURL: 'https://ppi-2.onrender.com/api', // Si no está definida la primera URL, usa localhost
     withCredentials: true
 });
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { connectDB } from './db.js';
 // Conectar a la base de datos
 connectDB();
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the API');
-  });
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the API');
+//   });
 
 // Utiliza el puerto que asigna Render o, en su defecto, el puerto 3000 para desarrollo local
 const PORT = process.env.PORT || 3000;
