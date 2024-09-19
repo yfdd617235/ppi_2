ab475eb19a3cff31e5f64709d70450ae0dbfc0bf
yfdd617235 <yosefdavid91@gmail.com>
2024-09-19 10:56:29 -0500

    navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
   const { isAuthenticated, logout, user } = useAuth();
 
   return (
-    <nav className="bg-black fixed top-0 left-1/2 transform -translate-x-1/2 w-full py-0 px-5 lg:px-10 z-50 border border-zinc-500">
+    <nav className="bg-black fixed top-0 left-1/2 transform -translate-x-1/2 w-full py-0 px-5 lg:px-10 z-50 border-b border-zinc-500">
       {/* <nav className="bg-black fixed top-0 left-1/2 transform -translate-x-1/2 w-full py-0 px-5 lg:px-10 z-50 border-2 border-zinc-800"> */}
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center">
