c460a0555d8c11a143e0e7925b0fcd8b544fddfe
yfdd617235 <yosefdavid91@gmail.com>
2024-09-24 11:29:44 -0500

    update token error

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,5 +1,11 @@
 import axios from './axios';
 
+import Cookies from 'js-cookie'; // Asegúrate de instalar 'js-cookie' si usas cookies
+
+export const getToken = () => {
+    return Cookies.get('token'); // O usa localStorage.getItem('token') si prefieres
+};
+
 export const registerRequest = user => axios.post(`/register`, user);
 
 export const loginRequest = user => axios.post(`/login`, user);
--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -16,38 +16,38 @@
 // }
 
 
-// import jwt from "jsonwebtoken"
-// import { TOKEN_SECRET } from "../config.js";
-
-// export const authRequired = (req, res, next) => {
-//     //console.log(req.headers);
-//     const { token } = req.cookies;
-//     if (!token) return res.status(401).json({ message: "No token, authorization denied" });
-
-//     jwt.verify(token, TOKEN_SECRET, (err, user) => {
-//         if (err) return res.status(403).json({ message: "Invalid token" });
-    
-//         req.user = user;
-//         next();
-//     });
-    
-// }
-
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"
 import { TOKEN_SECRET } from "../config.js";
 
 export const authRequired = (req, res, next) => {
-    // Obtener el token desde el encabezado 'Authorization'
-    const authHeader = req.headers['authorization']; // El encabezado tiene el formato "Bearer <token>"
-    const token = authHeader && authHeader.split(' ')[1]; // Si el encabezado existe, extraer el token
-
+    //console.log(req.headers);
+    const { token } = req.cookies;
     if (!token) return res.status(401).json({ message: "No token, authorization denied" });
 
-    // Verificar el token
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
         if (err) return res.status(403).json({ message: "Invalid token" });
-
-        req.user = user; // Añadir la información del usuario al request
+    
+        req.user = user;
         next();
     });
-};
+    
+}
+
+// import jwt from "jsonwebtoken";
+// import { TOKEN_SECRET } from "../config.js";
+
+// export const authRequired = (req, res, next) => {
+//     // Obtener el token desde el encabezado 'Authorization'
+//     const authHeader = req.headers['authorization']; // El encabezado tiene el formato "Bearer <token>"
+//     const token = authHeader && authHeader.split(' ')[1]; // Si el encabezado existe, extraer el token
+
+//     if (!token) return res.status(401).json({ message: "No token, authorization denied" });
+
+//     // Verificar el token
+//     jwt.verify(token, TOKEN_SECRET, (err, user) => {
+//         if (err) return res.status(403).json({ message: "Invalid token" });
+
+//         req.user = user; // Añadir la información del usuario al request
+//         next();
+//     });
+// };
