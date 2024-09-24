d7f1ba41f5e9ed6ce24c426c2d8415b2d4d3d720
yfdd617235 <yosefdavid91@gmail.com>
2024-09-24 08:38:14 -0500

    Profile Update

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -4,7 +4,7 @@ import Chart from '../components/Chart'; // Importa el componente PolarChart
 
 function ProfilePage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen mx-3 lg:m-16 ">
+    <div className="flex flex-col items-center justify-center min-h-screen mx-3 lg:m-16 my-12">
       <h2 className='font-bold '>Profile Status</h2> <br/>
       {/* Contenedor flex para los componentes */}
       <div className="flex flex-col items-center justify-center lg:flex-row lg:justify-between gap-10 w-full">
