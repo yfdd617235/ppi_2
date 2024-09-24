803bac10c23af72dc891ba8a22c63ced82094a1e
yfdd617235 <yosefdavid91@gmail.com>
2024-09-24 13:53:52 -0500

    update token error

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -22,7 +22,7 @@ import { TOKEN_SECRET } from "../config.js";
 export const authRequired = (req, res, next) => {
     //console.log(req.headers);
     const { token } = req.cookies;
-    if (!token) return res.status(401).json({ message: "No token, authorization denied" });
+    // if (!token) return res.status(401).json({ message: "No token, authorization denied" });
 
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
         if (err) return res.status(403).json({ message: "Invalid token" });
