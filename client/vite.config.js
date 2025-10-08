97e40526035dd3ab04f8920b2e56a70f1ae25baa
yfdd617235 <yosefdavid91@gmail.com>
2025-10-08 16:45:39 -0500

    github

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,17 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   // base: 'https://yfdd617235.github.io/ppi_2/', // Ruta base relativa
-//   base: '/ppi_2/', // Ruta base relativa
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/' , //Dominio personalizado
+  // base: 'https://yfdd617235.github.io/ppi_2/', // Ruta base relativa
+  base: '/ppi_2/', // Ruta base relativa
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/' , //Dominio personalizado
+// });
